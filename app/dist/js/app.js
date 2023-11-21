e1696c8d8df55fd4d392b145165b9eb45c966be2
Yasmin Sampaio <ssmysampaiio22@gmail.com>
2023-11-21 13:35:05 -0300

    criando conexão com a API

--- app/dist/js/app.js
+++ app/dist/js/app.js
@@ -10,3 +10,12 @@ if (form) {
 else {
     throw Error('Não foi possível inicializar a aplicação. Verifique se o form existe.');
 }
+const btnImporta = document.querySelector("#btn-import");
+if (btnImporta) {
+    btnImporta.addEventListener('click', event => {
+        controller.importaDados();
+    });
+}
+else {
+    throw Error('Não foi possível inicializar a aplicação. Verifique se o botão existe.');
+}
