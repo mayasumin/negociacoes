8cc72695c7b88c0b7ef0c4a131cc9125fa77064d
Yasmin Sampaio <ssmysampaiio22@gmail.com>
2023-11-21 09:20:30 -0300

    finalizando a decorator inspecionar e criando a decorator escapar

--- app/src/decorators/inspecionar.ts
+++ app/src/decorators/inspecionar.ts
@@ -1,14 +1,15 @@
-export function inspecionar() {
-    return function(
-        target: any,
-        propertyKey: string,
-        descriptor: PropertyDescriptor
-    ) {
-        const metodoOriginal = descriptor.value;
-        descriptor.value = function() {
-            
-        }
-
-        return descriptor;
-    }
+export function inspecionar(
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+) {
+    const metodoOriginal = descriptor.value;
+    descriptor.value = function (...args: any[]) {
+        console.log(`--- Método ${propertyKey}`);
+        console.log(`----- parâmetros: ${JSON.stringify(args)}`);
+        const retorno = metodoOriginal.apply(this, args);
+        console.log(`-------- retorno: ${JSON.stringify(retorno)}`);
+        return retorno;
+    };
+    return descriptor;
 }
--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -8,15 +8,15 @@ export function logarTempoDeExecucao(emSegundos: boolean = false) {
         descriptor.value = function(...args: any[]) {
             let divisor = 1;
             let unidade = 'milisegundos';
-            if(emSegundos) {
+            if (emSegundos) {
                 divisor = 1000;
                 unidade = 'segundos';
             }
-            const tempoInicial =  performance.now();
+            const t1 = performance.now();
             const retorno = metodoOriginal.apply(this, args);
-            const tempoFinal = performance.now()
-            console.log(`${propertyKey}, tempo de execução: ${(tempoFinal - tempoInicial)/divisor} ${unidade}`);
-            retorno;
+            const t2 = performance.now();
+            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1)/divisor} ${unidade}`);
+            retorno
         };
 
         return descriptor;
